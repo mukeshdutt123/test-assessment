8d2be02e8b4547f6ee83d648d4f11db4c2c085ad
mukeshdutt123 <37171498+mukeshdutt123@users.noreply.github.com>
2021-10-23 00:37:51 +0530

    Add files via upload

---
+++ nested- key-pair.py
@@ -0,0 +1,15 @@
+def returnobj(obj,key):
+obj1 = {}
+obj1 = obj
+keys = key.split('/')
+
+print(keys)
+ i=0
+ for k in keys:
+   for k[i] in obj1:
+   for k[i] == obj1.items(i):
+     continue
+Value = obj1[:- 1]
+return Value
+
+print(returnobj(obj,'x/y'))
